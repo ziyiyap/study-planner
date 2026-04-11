4d15bf1a076bbc2d9fb9871dc51d22b6b398f6ac
_ziyiyap <yapziyi13@gmail.com>
2026-04-12 00:11:40 +0800

    priority calculation

---
+++ services/scoring_service.py
@@ -0,0 +1,21 @@
+from models.database import db, Subjects
+from datetime import date
+
+def calculate_effective_difficulty(objective, passion):
+    return objective * (2 - passion)
+
+def calculate_priority(subject):
+    days_left = (subject.exam_date - date.today()).days
+    priority_score = (1-subject.progress) * (1/days_left) * subject.effective_difficulty * subject.importance_weight
+    return priority_score
+
+def get_all_priorities():
+    all_priorities = []
+    all_subjects = Subjects.query.all()
+
+    for subject in all_subjects:
+        score = calculate_priority(subject)
+        all_priorities.append((subject,score))
+
+    sorted_all = sorted(all_priorities, key=lambda x: x[1], reverse=True)
+    return sorted_all
